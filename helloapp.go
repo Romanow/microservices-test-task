2d0b1ed2ac2a2451fb380e88e7908bed2e776637
Alexander <onepopov@yandex.ru>
2022-07-27 18:02:49 +0400

    Remove exclamation point

--- helloapp.go
+++ helloapp.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", os.Getenv("GREETING_NAME"))
+		fmt.Fprintf(w, "Hello, %s", os.Getenv("GREETING_NAME"))
 	})
 	fmt.Printf("Server running on port 8080 \n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
