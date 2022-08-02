5567ea11e8f32939d7c6d09c8da73927643ad77d
arzmann <31392965+arzmann@users.noreply.github.com>
2022-08-02 10:18:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from os import getenv
 
 app = FastAPI()
 
-@app.api_route("/{path_name:path}")
+@app.api_route("/{path_name:path}", , response_class=PlainTextResponse)
 async def respond():
     return f"Hello, {getenv('GREETING_NAME', '')}"
